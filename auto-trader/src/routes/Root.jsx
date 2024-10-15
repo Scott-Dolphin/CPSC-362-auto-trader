0d717ee02b87886132173f8822b3193d722066bd
dylannwong <dylannwong@csu.fullerton.edu>
2024-10-14 22:50:11 -0700

    added dropdown and download button

--- auto-trader/src/routes/Root.jsx
+++ auto-trader/src/routes/Root.jsx
@@ -1,9 +1,25 @@
-
+import React from 'react';
+import {Button} from 'react-bootstrap';
 
 export default function Root() {
+
     return (
-        <>
-            <h1>Hello world</h1>
-        </>
+    <div style={{
+        display: "flex", 
+        justifyContent: "space-between", 
+        
+        width: "300px"
+            }}>
+
+
+        <select style={{background:"#103074", borderRadius: "10px"}}>
+
+        <option value="FNGU">FNGU</option>
+        <option value="FNGD">FNGD</option>
+
+        </select>
+
+      <Button style={{background: "#103074"}}>download</Button>
+    </div>
     );
 }
